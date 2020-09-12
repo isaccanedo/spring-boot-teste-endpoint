994f9e93dde5a91b1ebd04f15ba5a79320bda27c
isaccanedo <isaccanedo@gmail.com>
2020-09-12 12:05:56 -0300

    Initial commit

---
+++ src/main/java/br/com/isaccanedo/DemoApplication.java
@@ -0,0 +1,24 @@
+package br.com.isaccanedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	@ResponseBody
+	public String home() {
+		return "Testando o EndPoint na raiz do Servidor";
+
+	}
+
+}
